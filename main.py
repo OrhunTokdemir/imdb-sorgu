ab5a5beb1e298a5ae080389a30f8bbc8af5e85cc
OrhunTokdemir <148989093+OrhunTokdemir@users.noreply.github.com>
2025-05-19 00:07:59 +0300

    get resim üzeine çalışmaktayım

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import requests
 import http.client
 import json
+import sqlite3
+from tkinter import *
+from PIL import Image, ImageTk
 def get_anahtar():
     dosya=open('anahtar.txt',encoding='UTF-8', mode='r')
     anahtar=dosya.read()
     dosya.close()
     return str(anahtar)
 
-
-def get_resim(data):
+def download_resim(data):
     if not data['success']:
         print("Arama başarısız")
         return 0
@@ -23,23 +25,57 @@ def get_resim(data):
             except:
                 print("Resim kaydedilemedi")
                 return 0
+def get_resim(imdbID):
+    
+    try:
+        im = Image.open('./resim/' + imdbID + '.jpg')
+        img = ImageTk.PhotoImage(im.resize((64,64)))
+        return img
+    except:
+        print("Resim bulunamadı")
+        return None
+def get_data(film_adi):
+    conn = http.client.HTTPSConnection("api.collectapi.com")
+    headers = {
+        'content-type': "application/json",
+        'authorization': "apikey "+get_anahtar()
+        }
+    try:
+        conn.request("GET", "/imdb/imdbSearchByName?query="+film_adi.replace(" ","%20"), headers=headers)
+        res = conn.getresponse()
+        data = res.read()
+        data = json.loads(data)
+        return data
+    except Exception as e:
+        print("Hata:", e)
+        exit(1)
+def set_veritabani():
+    #veritabanı bağlantısı ve tablo oluşturma
+    root = sqlite3.connect('veri.db',isolation_level = None)
+    cursor = root.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS show (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            year TEXT,
+            imdbID TEXT NOT NULL,
+            type TEXT,
+            poster TEXT
+        )
+    ''')
+    root.commit()
+conn=set_veritabani
+data=get_data("cars")
 
-conn = http.client.HTTPSConnection("api.collectapi.com")
-headers = {
-     'content-type': "application/json",
-     'authorization': get_anahtar()
-     }
-try:
-    conn.request("GET", "/imdb/imdbSearchByName?query=cars", headers=headers)
-except Exception as e:
-    print("Hata:", e)
-    exit(1)
+img = get_resim("tt0071282")
+pencere = Tk()
+pencere.title("Film Arama")
+yazi = Label(pencere, text="Film Adı", bg="blue", anchor="w", image=img)
+yazi.image = img  # Keep a reference to the image
 
-res = conn.getresponse()
-data = res.read()
-data = json.loads(data)
-print(data)
-get_resim(data)
+yazi.pack(anchor="w")
+pencere.mainloop()
+
 
 
 
